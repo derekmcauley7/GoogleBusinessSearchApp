7cb367483356f71dd44093da1e365ea7a44e5c8a
Derek McAuley <derekmcauley7@gmail.com>
2019-11-11 23:00:24 +0000

    Updating review to connect to the database

---
+++ src/main/java/com/example/webservices/webrestful/ReviewRepository.java
@@ -0,0 +1,14 @@
+package com.example.webservices.webrestful;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, String> {
+
+        // custom query to search to review post by title or content
+        List<Review> findByplacesIdContainingOrContentContaining(String placesId, String placesIdAgain);
+
+    }
