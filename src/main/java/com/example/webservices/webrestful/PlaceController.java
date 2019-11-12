e9b64749d40d7f2e5ef803a9bb495a68d408c5ea
Derek McAuley <derekmcauley7@gmail.com>
2019-11-12 21:57:47 +0000

    Updating places bean to use JPA

--- src/main/java/com/example/webservices/webrestful/PlaceController.java
+++ src/main/java/com/example/webservices/webrestful/PlaceController.java
@@ -1,29 +1,47 @@
 package com.example.webservices.webrestful;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
-@CrossOrigin("http://localhost:4200")
 public class PlaceController {
 
-    @CrossOrigin
+    @Autowired
+    PlaceRepository placeRepository;
+
+    @GetMapping("/places")
+    public List<Place> index(){
+        return placeRepository.findAll();
+    }
+
+    @PostMapping("/place/search")
+    public List<Place> search(@RequestBody Map<String, String> body){
+        String searchTerm = body.get("text");
+        return placeRepository.findBynameContaining(searchTerm, searchTerm);
+    }
+
     @GetMapping(path = "/place" )
-    public PlaceBean helloWordBean(@RequestParam("search") String search){
-        PlaceBean place;
+    public Place create(@RequestParam("search") String search){
         ArrayList googleBusinessInfo = PlacesHelper.getPlaces(search);
         try {
-            place = new PlaceBean(googleBusinessInfo.get(0).toString(), googleBusinessInfo.get(1).toString(),
-                    googleBusinessInfo.get(2).toString(), googleBusinessInfo.get(3).toString(), googleBusinessInfo.get(4).toString());
-            return place;
+            String id = googleBusinessInfo.get(0).toString();
+            String name = googleBusinessInfo.get(1).toString();
+            String rating = googleBusinessInfo.get(3).toString();
+            String address = googleBusinessInfo.get(2).toString();
+            String image = googleBusinessInfo.get(4).toString();
+            return placeRepository.save(new Place(id, name, rating, address, image));
         } catch (Exception e) {
-            place = new PlaceBean("0", "No Listing Found",
-                    "", "","");
-            return place;
+            System.out.println(e);
+            return  null;
         }
     }
 
